97a7fbd4caa63c51e336a2bf1a211ffcf99eee2a
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-30 12:40:45 -0500

    updated store module

--- scripts/store.js
+++ scripts/store.js
@@ -2,9 +2,40 @@
 'use strict';
 
 const store = (function(){
-  const bookmarks = [];
-  const adding = false;
-  const minRating = null;
+  const addBookmark = function(data){
+    let bookmark =  {
+      editing: false,
+      expanded: false
+    };
+    Object.assign(bookmark, data);
+    this.bookmarks.push(bookmark);
+  };
+
+  const findById = function(id){
+    return this.bookmarks.find(mark => mark.id === id);
+  };
+
+  const findAndDelete = function(id){
+    this.bookmarks = this.bookmarks.filter(item => item.id !== id);
+  };
   
+  const setMinRating = function(rating){
+    this.minRating = rating;
+  };
+
+  const toggleAdding = function(){
+    this.adding = !this.adding;
+  };
+
+  return {
+    bookmarks: [],
+    adding: false,
+    minRating: null,
+    addBookmark,
+    findById,
+    findAndDelete,
+    setMinRating,
+    toggleAdding
+  };
 
 });
