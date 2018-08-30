d0f512860d213fcfd816133ef3e1cdd219b9916e
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-30 11:46:39 -0500

    added html for whole project and updated api.js

---
+++ scripts/api.js
@@ -0,0 +1,47 @@
+/* global $ */
+'use strict';
+
+const api = (function(){
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/cameron';
+
+  const getBookmarks = function(callback){
+    $.getJSON(`${BASE_URL}/bookmarks`, callback);
+  };
+
+  const createBookmark = function(data, success, error){
+    const newBookmark = JSON.stringify(data);
+    $.ajax({
+      url: `${BASE_URL}/bookmarks`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newBookmark,
+      success,
+      error
+    });
+  };
+
+  const deleteBookmark = function(id, success, error){
+    $.ajax({
+      url: `${BASE_URL}/bookmarks/${id}`,
+      method: 'DELETE',
+      success,
+      error
+    });
+  };
+  return {
+    getBookmarks,
+    createBookmark,
+    deleteBookmark
+  };
+})();
+
+// let book = {
+//   url: 'https://www.google.com/',
+//   title: 'Google',
+//   desc: 'great website',
+//   rating: 5
+// };
+
+// const callback = function(response){
+//   console.log(response);
+// };
---
+++ scripts/store.js
@@ -0,0 +1,10 @@
+/* global $ */
+'use strict';
+
+const store = (function(){
+  const bookmarks = [];
+  const adding = false;
+  const minRating = null;
+  
+
+});
